c0574753b8e1fd1e669a01b878baa021ecf049fb
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-01-21 16:10:48 +0100

    add time,temperature,description. humidity,wind

---
+++ src/app.js
@@ -0,0 +1,53 @@
+function formatDate(timestrap) {
+    let date=new Date(timestrap);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+
+}
+
+function displayTemperature(response) {
+console.log(response.data);    //mozemy sobie wpisywac zeby pozniej wyszukiwac rzeczy w kodzie
+   let temperatureElement=document.querySelector("#temperature");
+   let cityElement=document.querySelector("#city");
+   let descriptionElement=document.querySelector("#description");
+   let humidityElement=document.querySelector("#humidity");
+   let windElement=document.querySelector("#wind");
+   let dateElement=document.querySelector("#date");
+
+
+   temperatureElement.innerHTML=Math.round(response.data.main.temp); 
+   cityElement.innerHTML=response.data.name;
+   descriptionElement.innerHTML=response.data.weather[0].description;
+   humidityElement.innerHTML=response.data.main.humidity;
+   windElement.innerHTML=Math.round(response.data.wind.speed);
+   dateElement.innerHTML=formatDate(response.data.dt*1000);
+
+
+}
+let apiKey = "f58a3da8d9e0f160ba2b997349a49f23";
+let city="Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+
+  
+
+axios.get(apiUrl).then(displayTemperature)
