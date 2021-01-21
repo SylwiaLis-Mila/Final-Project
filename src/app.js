29635a6c41c09794612dfd00cb20f22ea756a76a
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-01-21 16:35:02 +0100

    add icon and img alt

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ console.log(response.data);    //mozemy sobie wpisywac zeby pozniej wyszukiwac r
    let humidityElement=document.querySelector("#humidity");
    let windElement=document.querySelector("#wind");
    let dateElement=document.querySelector("#date");
+   let iconElement=document.querySelector("#icon");
 
 
    temperatureElement.innerHTML=Math.round(response.data.main.temp); 
@@ -39,11 +40,15 @@ console.log(response.data);    //mozemy sobie wpisywac zeby pozniej wyszukiwac r
    humidityElement.innerHTML=response.data.main.humidity;
    windElement.innerHTML=Math.round(response.data.wind.speed);
    dateElement.innerHTML=formatDate(response.data.dt*1000);
-
-
+   iconElement.setAttribute(
+       "src",
+       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+   );
+   iconElement.setAttribute("alt", response.data.weather[0].description); //alt
 }
+
 let apiKey = "f58a3da8d9e0f160ba2b997349a49f23";
-let city="Paris";
+let city="Majorca";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
