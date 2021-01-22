b052cfbd1e8477bc08683edb71f329e14d31fe04
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-01-22 10:46:09 +0100

    add search html

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ function formatDate(timestrap) {
 }
 
 function displayTemperature(response) {
-console.log(response.data);    //mozemy sobie wpisywac zeby pozniej wyszukiwac rzeczy w kodzie
+//console.log(response.data);   
    let temperatureElement=document.querySelector("#temperature");
    let cityElement=document.querySelector("#city");
    let descriptionElement=document.querySelector("#description");
@@ -55,4 +55,4 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
 
   
 
-axios.get(apiUrl).then(displayTemperature)
+axios.get(apiUrl).then(displayTemperature);
