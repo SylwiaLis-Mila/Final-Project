936c11f1b6239216ea0b90ad1c8a642205f978e6
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-02-04 12:03:53 +0100

    add a revised version forecast (hours)

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ function formatDate(timestrap){
     let hours=date.getHours();
     let minutes=date.getMinutes();
     if(hours < 10){
-      hours=`0${minutes}`;
+      hours=`0${hours}`;
     }
      if(minutes < 10){
       minutes=`0${minutes}`;
